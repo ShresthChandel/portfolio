a940908609d0fd6c70a7718d2eac0e3251dc79b0
ShresthChandel <shresthsingh003@gmail.com>
2026-04-10 09:03:19 +0530

    Remove final Sentry trace from global error to fix Vercel build

--- app/global-error.jsx
+++ app/global-error.jsx
@@ -1,12 +1,12 @@
 "use client";
 
-import * as Sentry from "@sentry/nextjs";
 import Error from "next/error";
 import { useEffect } from "react";
 
 export default function GlobalError({ error }) {
   useEffect(() => {
-    Sentry.captureException(error);
+    // Log the error to your server or console
+    console.error(error);
   }, [error]);
 
   return (
